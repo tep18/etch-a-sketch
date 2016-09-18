4caed969831da771063e412e1b5e0cf73ddc3bc8
Tom <mr.tomplatts@gmail.com>
2016-09-17 23:33:02 -0700

    user can input grid size

--- js/jquery.js
+++ js/jquery.js
@@ -1,17 +1,47 @@
 $(document).ready(function() {
+
+	var blocksPerSide = 4;
+	var totalBlocks = 16;
 	
-	for (var i = 0; i < 16; i++) {
-		var divBlock = $('<div/>', {
-			"class": "block"
-		});
-		$('.container').append(divBlock);
+	function makeGrid() {
+		for (var i = 0; i < totalBlocks; i++) {
+			var divBlock = $('<div/>', {
+				"class": "block",
+				"id": i
+			});
+			$('.container').append(divBlock);
+			$('.block').css({
+				'height': 100/blocksPerSide + '%',
+				'width': 100/blocksPerSide + '%'
+			})
+		}
 	}
 
-	$('.block').on('mouseenter', function() {
-		$(this).addClass('highlight')
-	});
-	$('.block').on('click', function() {
-		$(this).toggleClass('highlight')
-	});
+	makeGrid();
+
+	function black() {
+
+		$('.block').on('mouseenter', function() {
+			$(this).addClass('highlight')
+		});
+
+		$('.block').on('click', function() {
+			$(this).toggleClass('highlight')
+		});
+	}
+
+	black()
+
+	$('button').on('click',function() {
+		$('html').find('.block').removeClass('highlight');
+
+		var usersize = prompt("How many squares would you like per side of the next grid?", "Go big...");
+		blocksPerSide = usersize;
+		totalBlocks = usersize ** 2;
+		$('.block').remove();
+		makeGrid();
+		black();
+
+	})
 
 });
