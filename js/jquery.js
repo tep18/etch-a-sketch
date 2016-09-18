d0766d433c4cb27a42d223f5c4be67af38dd81c3
Tom <mr.tomplatts@gmail.com>
2016-09-17 23:46:33 -0700

    optimised for speed

--- js/jquery.js
+++ js/jquery.js
@@ -5,15 +5,17 @@ $(document).ready(function() {
 	
 	function makeGrid() {
 		for (var i = 0; i < totalBlocks; i++) {
+			
 			var divBlock = $('<div/>', {
 				"class": "block",
-				"id": i
+				"id": i,
+				css:{
+					height: 100/blocksPerSide + '%',
+					width: 100/blocksPerSide + '%'
+				}
 			});
+
 			$('.container').append(divBlock);
-			$('.block').css({
-				'height': 100/blocksPerSide + '%',
-				'width': 100/blocksPerSide + '%'
-			})
 		}
 	}
 
@@ -35,7 +37,7 @@ $(document).ready(function() {
 	$('button').on('click',function() {
 		$('html').find('.block').removeClass('highlight');
 
-		var usersize = prompt("How many squares would you like per side of the next grid?", "Go big...");
+		var usersize = prompt("How many squares would you like per side of the next grid?", "Up to 64!");
 		blocksPerSide = usersize;
 		totalBlocks = usersize ** 2;
 		$('.block').remove();
